016f8cb7e0d250d2adc0aadb49fdb516185d0f70
shoney <ddmczp@naver.com>
2024-02-07 22:25:56 +0900

    fix: BasicLayout 수정

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -4,9 +4,7 @@ import BasicMenu from "../components/menus/BasicMenu";
 export default function BasicLayout({ children }) {
   return (
     <>
-      <header className="bg-teal-400 p-5">
-        <BasicMenu />
-      </header>
+      <BasicMenu />
       <div
         className="bg-white my-5 w-full flex flex-col space-y-4 md:flex-row md:space-x-4
         md:space-y-0"
