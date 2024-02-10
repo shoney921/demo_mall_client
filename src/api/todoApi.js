149864a34476447d9c2a7351379f1fff58b7b847
shoney <ddmczp@naver.com>
2024-02-10 16:02:17 +0900

    feat: axios를 사용해서 api 처리하는 js추가

---
+++ src/api/todoApi.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const API_SERVER_HOST = "http://localhost:8080";
+
+const prefix = `${API_SERVER_HOST}/api/todo`;
+
+export const getOne = async (tno) => {
+  const res = await axios.get(`${prefix}/${tno}`);
+  return res.data;
+};
+
+export const getList = async (pageParam) => {
+  const { page, size } = pageParam;
+  const res = await axios.get(`${prefix}/list`, { params: { page, size } });
+  return res.data;
+};
