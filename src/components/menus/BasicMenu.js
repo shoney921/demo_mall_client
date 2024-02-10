21be277299cc2b85b7d40d1ae2bf0cece2d35570
shoney <ddmczp@naver.com>
2024-02-10 21:56:29 +0900

    fix: 메뉴바 디자인 변경

--- src/components/menus/BasicMenu.js
+++ src/components/menus/BasicMenu.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function BasicMenu() {
   return (
     <nav id="navbar" className=" flex bg-black">
-      <div className="w-4/5 bg-gray-600">
+      <div className="w-full bg-gray-800">
         <ul className="flex p-4 text-white font-bold">
           <li className="pr-6 text-2xl">
             <Link to={"/"}>Main</Link>{" "}
@@ -17,8 +17,8 @@ export default function BasicMenu() {
           </li>
         </ul>
       </div>
-      <div className="w-1/5 flex justify-end bg-orange-200 p-4 font-medium">
-        <div className=" text-sm m-1 rounded">Login</div>
+      <div className="w-30 flex justify-end bg-orange-200 p-4 font-medium">
+        <div className=" text-sm m-1 rounded text-gray-800">Login</div>
       </div>
     </nav>
   );
