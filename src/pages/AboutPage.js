3ac5d8e91c7a590ef4f0a8096f9daa51cdf79530
shoney <ddmczp@naver.com>
2024-02-18 22:06:46 +0900

    fix: about page는 로그인 해야지 보이도록 수정

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import useCustomLogin from "../hooks/useCustomLogin";
 import BasicLayout from "../layouts/BasicLayout";
 
 export default function AboutPage() {
+  const { isLogin, moveToLoginReturn } = useCustomLogin();
+
+  if (!isLogin) {
+    return moveToLoginReturn();
+  }
   return (
     <BasicLayout>
       <div className={"text-3xl"}>
