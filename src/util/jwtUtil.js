0f2bd1b63d421162609429ddb165a0291fbe9a9f
shoney <ddmczp@naver.com>
2024-03-01 17:02:25 +0900

    chore: Axios 인터셉터에서 Refresh 토큰으로 Access 토큰 자동 갱신 로직 구현

--- src/util/jwtUtil.js
+++ src/util/jwtUtil.js
@@ -1,7 +1,19 @@
 import axios from "axios";
-import { getCookie } from "./cookieUtil";
+import { getCookie, setCookie } from "./cookieUtil";
+import { API_SERVER_HOST } from "../api/todoApi";
 
 const jwtAxios = axios.create();
+
+const refreshJWT = async (accessToken, refreshToken) => {
+  const host = API_SERVER_HOST;
+  const header = { headers: { Authorization: `Bearer ${accessToken}` } };
+  const res = await axios.get(
+    `${host}/api/member/refresh?refreshToken=${refreshToken}`,
+    header
+  );
+  console.log(res.data);
+  return res.data;
+};
 
 //before request
 const beforeReq = (config) => {
@@ -30,6 +42,28 @@ const requestFail = (err) => {
 //before return response
 const beforeRes = async (res) => {
   console.log("before return response...........");
+
+  const data = res.data;
+
+  if (data && data.error === "ERROR_ACCESS_TOKEN") {
+    const memberCookieValue = getCookie("member");
+
+    const result = await refreshJWT(
+      memberCookieValue.accessToken,
+      memberCookieValue.refreshToken
+    );
+
+    //새로운 토큰으로 쿠키 갱신 필요
+    memberCookieValue.accessToken = result.accessToken;
+    memberCookieValue.refreshToken = result.refreshToken;
+    setCookie("member", JSON.stringify(memberCookieValue), 1);
+
+    // 새로운 액세스 토큰으로 원래 하려고 했던 작업을 다시 작업
+    const originalRequest = res.config;
+    originalRequest.headers.Authorization = `Bearer ${result.accessToken}`;
+
+    return await axios(originalRequest);
+  }
   return res;
 };
 
