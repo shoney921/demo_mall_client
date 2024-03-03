55c89917ac093be696f0727a033d9ff295d4cbe6
shoney <ddmczp@naver.com>
2024-03-03 20:15:05 +0900

    chore: Kakao Access Token 받는 부분 추가

--- src/api/kakaoApi.js
+++ src/api/kakaoApi.js
@@ -1,11 +1,33 @@
+import axios from "axios";
 import React from "react";
 
 const rest_api_key = "59ff3eb95f7f6ae20d0c8a53af4cfc85";
 const redirect_uri = "http://localhost:3000/member/kakao";
 const auth_code_path = "https://kauth.kakao.com/oauth/authorize";
+const access_token_url = "https://kauth.kakao.com/oauth/token";
 
 export const getKakaoLoginLink = () => {
   const kakaoURL = `${auth_code_path}?client_id=${rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
-
   return kakaoURL;
+};
+
+export const getAccessToken = async (authCode) => {
+  const header = {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+    },
+  };
+
+  const params = {
+    grant_type: "authorization_code",
+    client_id: rest_api_key,
+    redirect_uri: redirect_uri,
+    code: authCode,
+  };
+
+  const res = await axios.post(access_token_url, params, header);
+
+  const accessToken = res.data.access_token;
+
+  return accessToken;
 };
--- src/pages/member/KakaoRedirectPage.js
+++ src/pages/member/KakaoRedirectPage.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
+import { getAccessToken } from "../../api/kakaoApi";
 
 export default function KakaoRedirectPage() {
   const [searchParams] = useSearchParams();
 
   const authCode = searchParams.get("code");
+
+  useEffect(() => {
+    getAccessToken(authCode).then((data) => {
+      console.log(data);
+    });
+  }, [authCode]);
   return (
     <div>
       <div>Kakao Login Redirect</div>
