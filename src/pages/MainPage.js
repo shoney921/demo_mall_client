5b8054354023917c2d6302f5500e4e23de2adef3
shoney <ddmczp@naver.com>
2024-03-17 18:24:17 +0900

    fix: 메인페이지 설명 추가

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -5,8 +5,14 @@ import BasicLayout from "../layouts/BasicLayout";
 const MainPage = () => {
   return (
     <BasicLayout>
-      <div className={"text-3xl"}>
+      <div className="text-3xl">
         <div>Main Page</div>
+        <div className="text-xl">
+          <div>
+            템플릿 용으로 만든 페이지 입니다. 메인페이지에 올리고 싶은 내용을
+            올리는 공간입니다.
+          </div>
+        </div>
         <div>
           <Link to={"/about"}>go about</Link>
         </div>
