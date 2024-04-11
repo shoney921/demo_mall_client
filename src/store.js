2af7a080425730923813ccd5c2c0906b9eacfe5d
shoney <ddmczp@naver.com>
2024-04-11 14:14:47 +0900

    feat: cart redux 사용하기 위해, cartSlice, store 추가

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "./slices/cartSlice";
 import loginSlice from "./slices/loginSlice";
 
 export default configureStore({
   reducer: {
     loginSlice: loginSlice,
+    cartSlice: cartSlice,
   },
 });
