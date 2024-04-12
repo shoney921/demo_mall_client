2e7963af33c8aecd5cc0935f62ef95fe2213835e
shoney <ddmczp@naver.com>
2024-04-12 13:11:55 +0900

    style: 메인 사진 변경

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -1,19 +1,20 @@
 import React from "react";
 import BasicMenu from "../components/menus/BasicMenu";
+import CartComponent from "../components/menus/CartComponent";
 
 export default function BasicLayout({ children }) {
   return (
     <>
       <BasicMenu />
       <div
-        className="bg-white my-5 w-full flex flex-col space-y-4 md:flex-row md:space-x-2
+        className="bg-white my-5 w-full flex flex-col md:flex-row md:space-x-2
         md:space-y-0"
       >
-        <main className="border-black border-2 md:w-2/3 lg:w-3/4 px-5 py-40">
+        <div className="border-black border-2 md:w-2/3 lg:w-3/4 justify-start">
           {children}
-        </main>
+        </div>
         <aside className="border-black border-2 md:w-1/3 lg:w-1/4 px-5 py-40">
-          <h1 className="text-2xl md:text-4xl"> Sidebar </h1>
+          <CartComponent />
         </aside>
       </div>
     </>
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,21 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import BasicLayout from "../layouts/BasicLayout";
+import mainImg from "../styles/Main Page2.png";
 
 const MainPage = () => {
   return (
     <BasicLayout>
-      <div className="text-3xl">
-        <div>Main Page</div>
-        <div className="text-xl">
-          <div>
-            템플릿 용으로 만든 페이지 입니다. 메인페이지에 올리고 싶은 내용을
-            올리는 공간입니다.
-          </div>
-        </div>
-        <div>
-          <Link to={"/about"}>go about</Link>
-        </div>
+      <img src={mainImg} alt="mainImg" />
+      <div>
+        <Link to={"/about"}>go about</Link>
       </div>
     </BasicLayout>
   );
