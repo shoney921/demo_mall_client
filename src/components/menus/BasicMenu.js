542f3169cb67fc9118ff06b3b961a49a362ac991
shoney <ddmczp@naver.com>
2024-04-12 13:19:59 +0900

    style: 베이직 메뉴 색상 변경

--- src/components/menus/BasicMenu.js
+++ src/components/menus/BasicMenu.js
@@ -6,7 +6,7 @@ export default function BasicMenu() {
   const loginState = useSelector((state) => state.loginSlice);
   return (
     <nav id="navbar" className=" flex bg-black">
-      <div className="w-full bg-gray-800">
+      <div className="w-full bg-black">
         <ul className="flex p-4 text-white font-bold">
           <li className="pr-6 text-2xl">
             <Link to={"/"}>Main</Link>{" "}
@@ -29,7 +29,7 @@ export default function BasicMenu() {
           )}
         </ul>
       </div>
-      <div className="w-30 flex justify-end bg-orange-200 p-4 font-medium">
+      <div className="w-30 flex justify-end bg-slate-300 p-4 font-medium">
         <div className=" text-sm m-1 rounded text-gray-800">
           {loginState.id ? (
             <Link to={"/member/logout"}>Logout</Link>
