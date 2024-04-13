7fbc1d5945933d06db0d54c739643ab80aaa235a
shoney <ddmczp@naver.com>
2024-04-13 18:07:49 +0900

    feat: 상세 상품 화면에서 카드 담기 버튼 추가

--- src/components/products/ReadComponent.js
+++ src/components/products/ReadComponent.js
@@ -3,6 +3,8 @@ import { getOne } from "../../api/productsApi";
 import { API_SERVER_HOST } from "../../api/todoApi";
 import FetchingModal from "../common/FetchingModal";
 import useCustomMove from "../../hooks/useCustomMove";
+import useCustomCart from "../../hooks/useCustomCart";
+import useCustomLogin from "../../hooks/useCustomLogin";
 
 const host = API_SERVER_HOST;
 
@@ -18,6 +20,8 @@ export default function ReadComponent({ pno }) {
   const [product, setProduct] = useState(initState);
   const [fetiching, setFetching] = useState(false);
   const { moveToList, moveToModify } = useCustomMove();
+  const { cartItems, changeCart } = useCustomCart();
+  const { loginState } = useCustomLogin();
 
   useEffect(() => {
     setFetching(true);
@@ -26,6 +30,23 @@ export default function ReadComponent({ pno }) {
       setFetching(false);
     });
   }, [pno]);
+
+  const handleClickAddCart = () => {
+    let qty = 1;
+
+    const addedItem = cartItems.filter((item) => item.pno === parseInt(pno))[0]; // 이미 카트에 추가되어있는 아이템
+
+    if (addedItem) {
+      if (
+        window.confirm("이미 추가된 상품입니다. 추가하시겠습니까?") === false
+      ) {
+        return;
+      }
+      qty = addedItem.qty + 1;
+    }
+
+    changeCart({ memberId: loginState.id, qty: qty, pno: pno });
+  };
 
   return (
     <div>
@@ -45,6 +66,12 @@ export default function ReadComponent({ pno }) {
       ))}
       {fetiching ? <FetchingModal /> : <></>}
       <div className="flex flex-row justify-end">
+        <button
+          className="bg-green-300 m-2 p-2 w-20"
+          onClick={handleClickAddCart}
+        >
+          Add Cart
+        </button>
         <button
           className="bg-red-300 m-2 p-2 w-20"
           onClick={() => moveToModify(pno)}
