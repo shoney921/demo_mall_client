2db9c5deab4d23be14d9f0b80c49ec287ec5c501
shoney <ddmczp@naver.com>
2024-04-14 13:34:28 +0900

    chore: App.js 에 리액트 쿼리 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { QueryClient } from "@tanstack/query-core";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { root } from "./router/root";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClinet = new QueryClient();
 
 function App() {
-  return <RouterProvider router={root} />;
+  return (
+    <QueryClientProvider client={queryClinet}>
+      <RouterProvider router={root} />
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
