f27bc6c2243b445a105024a494902ff137d9c986
한주연 <hjy2859@naver.com>
2024-04-14 18:02:10 +0900

    fix: productList useQuery 적용

--- src/components/products/ListComoponet.js
+++ src/components/products/ListComoponet.js
@@ -4,72 +4,72 @@ import useCustomMove from "../../hooks/useCustomMove";
 import FetchingModal from "../common/FetchingModal";
 import PageComponent from "../common/PageComponent";
 import useCustomLogin from "../../hooks/useCustomLogin";
-import { API_SERVER_HOST } from "../../util/constants";
+import { API_SERVER_HOST, QUERY_KEYS } from "../../util/constants";
+import { useQuery } from "@tanstack/react-query";
 
 const host = API_SERVER_HOST;
 
-const initState = {
-  dtoList: [],
-  pageNumList: [],
-  pageReqDto: null,
-  prev: false,
-  next: false,
-  totalCount: 0,
-  prevPage: 0,
-  nextPage: 0,
-  totalPage: 0,
-  current: 0,
-};
-
 export default function ListComoponet() {
   const { exceptionHandle } = useCustomLogin();
-  const { moveToList, moveToRead, page, size, refresh } = useCustomMove();
-  const [serverData, setServerData] = useState(initState);
-  const [fetiching, setFetching] = useState(false);
+  const { moveToList, moveToRead, page, size } = useCustomMove();
 
-  useEffect(() => {
-    setFetching(true);
-    getList({ page, size })
-      .then((data) => {
-        setFetching(false);
-        setServerData(data);
-      })
-      .catch((err) => exceptionHandle(err));
-  }, [page, size, refresh]);
+  const {
+    data: serverData,
+    isFetching,
+    isError,
+    error,
+    isSuccess,
+  } = useQuery({
+    queryKey: [QUERY_KEYS.GET_PRODUCTS_LIST, { page, size }],
+    queryFn: () => getList({ page, size }),
+    staleTime: 1000,
+  });
+
+  if (isFetching) return <FetchingModal />;
+
+  if (isError) {
+    exceptionHandle(error);
+  }
 
   return (
     <div className="border-2 border-blue-100 mt-10 mr-2 ml-2">
-      <div className="flex flex-wrap mx-auto p-6">
-        {serverData.dtoList &&
-          serverData.dtoList.map((product) => (
-            <div
-              key={product.pno}
-              className="w-1/2 p-1 rounded shadow-md border-2"
-              onClick={() => moveToRead(product.pno)}
-            >
-              <div className="flex flex-col h-full">
-                <div className="font-extrabold text-2xl p-2 w-full ">
-                  {product.pno}
-                </div>
-                <div className="text-1xl m-1 p-2 w-full flex flex-col">
-                  <div className="w-full overflow-hidden ">
-                    <img
-                      alt="product"
-                      className="m-auto rounded-md w-30"
-                      src={`${host}/api/products/view/s_${product.uploadedFileNames[0]}`}
-                    />
+      {isSuccess && (
+        <>
+          <div className="flex flex-wrap mx-auto p-6">
+            {serverData.dtoList.map((product) => (
+              <div
+                key={product.pno}
+                className="w-1/2 p-1 rounded shadow-md border-2"
+                onClick={() => moveToRead(product.pno)}
+              >
+                <div className="flex flex-col h-full">
+                  <div className="font-extrabold text-2xl p-2 w-full ">
+                    {product.pno}
                   </div>
-                  <div className="bottom-0 font-extrabold bg-white">
-                    <div className="text-center p-1">이름: {product.pname}</div>
-                    <div className="text-center p-1">가격: {product.price}</div>
+                  <div className="text-1xl m-1 p-2 w-full flex flex-col">
+                    <div className="w-full overflow-hidden ">
+                      <img
+                        alt="product"
+                        className="m-auto rounded-md w-30"
+                        src={`${host}/api/products/view/s_${product.uploadedFileNames[0]}`}
+                      />
+                    </div>
+                    <div className="bottom-0 font-extrabold bg-white">
+                      <div className="text-center p-1">
+                        이름: {product.pname}
+                      </div>
+                      <div className="text-center p-1">
+                        가격: {product.price}
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
-      </div>
-      <PageComponent serverData={serverData} movePage={moveToList} />
-      {fetiching ? <FetchingModal /> : <></>}
+            ))}
+          </div>
+          <PageComponent serverData={serverData} movePage={moveToList} />
+        </>
+      )}
     </div>
   );
 }
